3b79596d427ffbc2baf657c429335ecbd83ca2f3
Saskia <131782929+SaskiaJuergens@users.noreply.github.com>
2024-10-03 21:40:58 +0200

    sniffer demo 1.0

--- sniffer/sniffer-demo.py
+++ sniffer/sniffer-demo.py
@@ -2,6 +2,12 @@ import socket   #bietet Zugang zu Low-Level-Netzwerk-Sockets und verschiedene Ne
 import struct   #Ermöglicht das Packen und Entpacken von binären Daten in Python
 import textwrap #Text umzubrechen oder zu formatieren
 
+def main():
+    connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = connection.recvfrom(65536)
+
 #unpack ethernet frame das im Eingabewert data derfuntion gegeben wird
 #die ersten 14 Bytes des Frames (Ziel-Window-Adress, Quell-Window-Adresse, Protokolltyp) 
 #!: Das "Netzwerk-Big-Endian" => eine Byte-Reihenfolge, bei der das wichtigste Byte zuerst kommt
@@ -11,4 +17,7 @@ def etthnet_frame(data):
     destination_window, src_window, proto = struct.unpack('! 6s 6s H', data[:14])   
     return get_window_addr(destination_window), get_window_addr(src_window), socket.htons(proto), data[14:]
 
-#def get_window_addr():
+#Return properly formated Window address (AA:BB:CC:DD:EE:FF)
+def get_window_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
